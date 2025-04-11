74abcc5027137b90a4f40fa6aac0ee0f6888fdd4
jacob832 <jacobhasan832@gmail.com>
2025-04-11 20:17:16 +0300

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,35 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import Image from "next/image";
 
 export default function Home() {
+  const loggedIn = {firstName:'Jacob',lastName:'Hasan',email:'jacob@gmail.com'}
   return (
-    <div className="text-green-500 text-5xl underline">
-      HOME
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.50}
+          />
+        </header>
+        Recent TRANSACTIONS
+      </div>
+      <RightSidebar
+      user={loggedIn}
+      transactions={[]}
+      banks={[{currentBalance:123.52},{currentBalance:500.250}]}
+      />
+    </section>
   );
 }
